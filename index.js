e31049136cb52c14fea01d84a77ae680901b6110
Your Name <you@example.com>
2024-04-04 09:28:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var http = require("http");
+var fs = require("fs");
+
+var data = {
+  id: 456,
+  name: "Node.js",
+  category: "js",
+};
+
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      var html = fs.readFileSync("./index.html", "utf-8");
+
+      res.end(html);
+    } else if (req.url === "/quiz") {
+      res.writeHead(200, { "content-type": "text/html" });
+      var html = fs.readFileSync("./quiz.html", "utf-8");
+      res.end(html);
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.end(JSON.stringify(data));
+    }
+  })
+  .listen(3000);
+
+console.log("✔ Connected to server");
