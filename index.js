d841afd38217a829acf967f962721eebb45e6358
Your Name <you@example.com>
2024-04-05 09:08:06 +0200

    create small database and finish view

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-var http = require("http");
-var fs = require("fs");
+function alert() {
+  Swal.fire({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success",
+  });
+}
 
-var data = {
-  id: 456,
-  name: "Node.js",
-  category: "js",
-};
+//https://www.youtube.com/watch?v=LJ4PZ8JnRCg
 
-http
-  .createServer((req, res) => {
-    if (req.url === "/") {
-      res.writeHead(200, { "content-type": "text/html" });
-      var html = fs.readFileSync("./index.html", "utf-8");
+// var http = require("http");
+// var fs = require("fs");
 
-      res.end(html);
-    } else if (req.url === "/quiz") {
-      res.writeHead(200, { "content-type": "text/html" });
-      var html = fs.readFileSync("./quiz.html", "utf-8");
-      res.end(html);
-    } else {
-      res.writeHead(404, { "content-type": "text/html" });
-      res.end(JSON.stringify(data));
-    }
-  })
-  .listen(3000);
+// var data = {
+//   id: 456,
+//   name: "Node.js",
+//   category: "js",
+// };
 
-console.log("✔ Connected to server");
+// http
+//   .createServer((req, res) => {
+//     if (req.url === "/") {
+//       res.writeHead(200, { "content-type": "text/html" });
+//       var html = fs.readFileSync("./index.html", "utf-8");
+
+//       res.end(html);
+//     } else if (req.url === "/quiz") {
+//       res.writeHead(200, { "content-type": "text/html" });
+//       var html = fs.readFileSync("./quiz.html", "utf-8");
+//       res.end(html);
+//     } else {
+//       res.writeHead(404, { "content-type": "text/html" });
+//       res.end(JSON.stringify(data));
+//     }
+//   })
+//   .listen(3000);
+
+// console.log("✔ Connected to server");
