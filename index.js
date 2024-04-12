90418726be823b4889f075d8cd80794d2b8809d8
Fabian <102579957+Mrsfab09@users.noreply.github.com>
2024-04-12 05:47:52 +0000

    install json-server

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function results() {
-  Swal.fire({
-    title: "Good job!",
-    text: "You clicked the button!",
-    icon: "success",
-  });
-}
+
 
 // https://www.youtube.com/watch?v=LJ4PZ8JnRCg
--- server.js
+++ server.js
@@ -1,45 +1,31 @@
-const http = require("http");
-const fs = require("fs");
+var http = require("http");
+var fs = require("fs");
 
-http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    const html = fs.readFileSync("./index.html", "utf-8");
-    res.end(html);
-  } else if (req.url === "/quiz") {
-    fs.readFile("db.json", "utf-8", (err, jsonData) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Błąd odczytu pliku JSON");
-        return;
-      }
-      try {
-        const questionsData = JSON.parse(jsonData);
-        fs.readFile("quiz.html", "utf-8", (err, quizHtml) => {
-          if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("Błąd odczytu pliku HTML");
-            return;
-          }
-          res.writeHead(200, { "content-type": "text/html" });
-          let html = "<h1>Quiz</h1>";
-          html += "<ol>";
-          questionsData.questions.forEach((question, index) => {
-            html += `<li>${question.header}</li>`;
-          });
-          html += "</ol>";
-          html += quizHtml;
-          res.end(html);
-        });
-      } catch (error) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Błąd parsowania danych JSON");
-      }
-    });
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.end("<h1>404 Not Found</h1>");
-  }
-}).listen(3000);
+var data = {
+  id: 456,
+  name: "Node.js",
+  category: "js",
+};
+
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      var html = fs.readFileSync("./index.html", "utf-8");
+
+      res.end(html);
+    } else if (req.url === "/quiz") {
+      res.writeHead(200, { "content-type": "text/html" });
+      var html = fs.readFileSync("./quiz.html", "utf-8");
+      res.end(html);
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.end(JSON.stringify(data));
+    }
+  })
+  .listen(3000);
 
 console.log("✔ Connected to server");
+
+
+//https://www.youtube.com/watch?v=PBcqGxrr9g8
