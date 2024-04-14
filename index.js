53ff24cbd519695000bf48a189e3df76cc622e88
MrsFab09 <ytfabiskora07@gmail.com>
2024-04-14 22:08:26 +0200

    add new style and install supabase

--- index.js
+++
@@ -1,3 +0,0 @@
-
-
-// https://www.youtube.com/watch?v=LJ4PZ8JnRCg
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var http = require("http");
 var fs = require("fs");
+var html = fs.readFileSync("./public/script.js", "utf-8");
+
+var data = {
+  id: 456,
+  name: "Node.js",
+  category: "js",
+};
+
+var http = require("http");
+var fs = require("fs");
 
 var data = {
   id: 456,
@@ -12,12 +22,22 @@ http
     if (req.url === "/") {
       res.writeHead(200, { "content-type": "text/html" });
       var html = fs.readFileSync("./index.html", "utf-8");
-
       res.end(html);
     } else if (req.url === "/quiz") {
       res.writeHead(200, { "content-type": "text/html" });
       var html = fs.readFileSync("./quiz.html", "utf-8");
       res.end(html);
+    } else if (req.url === "/public/script.js") {
+      // Obsługa żądania dla pliku script.js
+      fs.readFile("./public/script.js", (err, data) => {
+        if (err) {
+          res.writeHead(404, { "content-type": "text/plain" });
+          res.end("File not found");
+        } else {
+          res.writeHead(200, { "content-type": "text/javascript" });
+          res.end(data);
+        }
+      });
     } else {
       res.writeHead(404, { "content-type": "text/html" });
       res.end(JSON.stringify(data));
@@ -26,6 +46,5 @@ http
   .listen(3000);
 
 console.log("✔ Connected to server");
-
 
 //https://www.youtube.com/watch?v=PBcqGxrr9g8
